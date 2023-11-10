26c6a1fad7d22f5a88ec58955425c2769c4cec27
Ratnesh Mether <mywork.ratnesh@gmail.com>
2023-11-10 22:11:49 +0530

    [25] : Check In.

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ import store from "./Redux-Store/store";
   5. Complete Task. - In Progress...
   6. Add CSS. - In Progress.
   7. Optimise Context - Done.
-  8. Introduce Redux - Done
+  8. Introduce Redux - Done.
 */
 
 export default function App() {
